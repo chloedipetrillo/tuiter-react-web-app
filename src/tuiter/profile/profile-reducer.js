24ae902b458999b8f05e6afd5aa181d4e6e62764
chloedipetrillo <dipetrillo.c@northeastern.edu>
2023-03-19 14:11:41 -0400

    edit

--- src/tuiter/profile/profile-reducer.js
+++ src/tuiter/profile/profile-reducer.js
@@ -3,9 +3,9 @@ import profArray from './profile.json';
 
 const prof = {
     "_id": 123,
-    "name": "Chloe DiPetrillo",
-    "handle": "@chlodip",
-    "profilePicture": "https://imageio.forbes.com/specials-images/imageserve/dv424076/0x0.jpg?format=jpg&width=1200",
+    "name": "Rock",
+    "handle": "@rock",
+    "profilePicture": "https://betterpet.com/wp-content/uploads/2022/08/Shih-tzu-funfacts.jpeg",
     "bannerPicture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOKfRTuGmudOek5wwrXoC4vAFAANBFv7N17nezL6Ow&s",
     "bio": "It's not easy being this famous. Follow for daily knock-knock jokes!",
     "website": "youtube.com/webdevtv",
